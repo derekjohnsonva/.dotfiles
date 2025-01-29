6974eebc2e0c2ad3e6b6b4c8016b158971f721f2
Derek Johnson <dej3tc@virginia.edu>
2025-01-29 09:11:51 -0700

    add aerc support

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-# things that need to be installed 
-# nvim 
+# things that need to be installed
+# nvim
 # stow
 # zsh
 
-
 # install antigen
-curl -L git.io/antigen > antigen.zsh
+curl -L git.io/antigen >antigen.zsh
 
 # stow dotfiles
 stow git
 stow vim
 stow nvim
 stow zsh
+stow aerc
 
 # add zsh as a login shell
 command -v zsh | sudo tee -a /etc/shells
@@ -19,11 +19,10 @@ command -v zsh | sudo tee -a /etc/shells
 # use zsh as default shell
 sudo chsh -s $(which zsh) $USER
 
-
 # install neovim plugins
 nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
 # Install vundle for vim
 git clone https://github.com/VundleVim/Vundle.vim.git vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 # Use kitty terminal on MacOS
-[ `uname -s` = 'Darwin' ] && stow kitty
+[ $(uname -s) = 'Darwin' ] && stow kitty
