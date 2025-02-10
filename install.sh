cf13ea65f3947d5d5842c47b2b435ee0d38a4913
Derek Johnson <dej3tc@virginia.edu>
2025-02-10 13:02:53 -0500

    add packages to install script

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 # things that need to be installed
-# nvim
 # stow
+# git
 # zsh
+# nvim
+# # Things that Nvim needs
+# # lazygit
+# # fzf
+# # ripgrep
+# If it is ubuntu this is what you would need to run
+apt update
+apt install git zsh neovim stow curl -y
 
 # install antigen
 curl -L git.io/antigen >antigen.zsh
@@ -19,10 +27,6 @@ command -v zsh | sudo tee -a /etc/shells
 # use zsh as default shell
 sudo chsh -s $(which zsh) $USER
 
-# install neovim plugins
-nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
 # Install vundle for vim
 git clone https://github.com/VundleVim/Vundle.vim.git vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-# Use kitty terminal on MacOS
-[ $(uname -s) = 'Darwin' ] && stow kitty
